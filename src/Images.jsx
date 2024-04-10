1d60e06d15dede4b3dd67965780aae77fd2e237f
sandeep-233 <sandeepkv233@gmail.com>
2024-04-10 19:45:30 +0530

    added another image

--- src/Images.jsx
+++ src/Images.jsx
@@ -3,6 +3,7 @@ import img1 from './assets/img1.png';
 import img2 from './assets/img2.png';
 import img3 from  './assets/img3.png';
 import img4 from  './assets/img4.png';
+import img5 from './assets/bookStore_img.png'
 
 
 export const Images = () => {
@@ -11,7 +12,8 @@ export const Images = () => {
         <img src={img1} alt=""  className='w-[180px] h-[180px]'/>
         <img src={img2} alt=""  className='w-[180px] h-[180px]'/>
         <img src={img3} alt="" className='w-[180px] h-[180px]' />
-        <img src={img4} alt="" className='w-[180px] h-[180px]' />v
+        <img src={img4} alt="" className='w-[180px] h-[180px]' />
+        <img src={img5} alt="" className='w-[180px] h-[180px]' />
     </div>
   )
 }
