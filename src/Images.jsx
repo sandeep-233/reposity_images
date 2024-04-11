0b9848cb26d6188c955b47538442c197238daaf0
sandeep-233 <sandeepkv233@gmail.com>
2024-04-11 08:04:42 +0530

    added another images

--- src/Images.jsx
+++ src/Images.jsx
@@ -3,17 +3,41 @@ import img1 from './assets/img1.png';
 import img2 from './assets/img2.png';
 import img3 from  './assets/img3.png';
 import img4 from  './assets/img4.png';
+
 import img5 from './assets/bookStore_img.png'
 
+import img6 from './assets/assesment_img1.png'
+import img7 from './assets/assesment_img2.png'
+
+import img8 from './assets/devDetactive_img1.png'
+import img9 from './assets/devDetactive_img2.png'
+
+import img10 from './assets/space_img_1.png'
+import img11 from './assets/space_img_2.png'
+import img12 from './assets/space_img_3.png'
+import img13 from './assets/space_img_4.png'
 
 export const Images = () => {
   return (
     <div className='flex flex-wrap gap-3'>
-        <img src={img1} alt=""  className='w-[180px] h-[180px]'/>
-        <img src={img2} alt=""  className='w-[180px] h-[180px]'/>
-        <img src={img3} alt="" className='w-[180px] h-[180px]' />
-        <img src={img4} alt="" className='w-[180px] h-[180px]' />
-        <img src={img5} alt="" className='w-[180px] h-[180px]' />
+        <img src={img1} alt=""  className='w-[1080px] h-[1080px]'/>
+        <img src={img2} alt=""  className='w-[1080px] h-[1080px]'/>
+        <img src={img3} alt="" className='w-[1080px] h-[1080px]' />
+        <img src={img4} alt="" className='w-[1080px] h-[1080px]' />
+
+        <img src={img5} alt="" className='w-[1080px] h-[1080px]' />
+
+        <img src={img6} alt="" className='w-[1080px] h-[1080px]' />
+        <img src={img7} alt="" className='w-[1080px] h-[1080px]' />
+
+        <img src={img8} alt="" className='w-[1080px] h-[1080px]' />
+        <img src={img9} alt="" className='w-[1080px] h-[1080px]' />
+
+        <img src={img10} alt="" className='w-[1080px] h-[1080px]' />
+        <img src={img11} alt="" className='w-[1080px] h-[1080px]' />
+        <img src={img12} alt="" className='w-[1080px] h-[1080px]' />
+        <img src={img13} alt="" className='w-[1080px] h-[1080px]' />
+
     </div>
   )
 }
