5655ee47a0e71286b12b4d357e918db459e6ede6
sandeep-233 <sandeepkv233@gmail.com>
2024-04-11 14:28:40 +0530

    added one more image

--- src/Images.jsx
+++ src/Images.jsx
@@ -17,6 +17,8 @@ import img11 from './assets/space_img_2.png'
 import img12 from './assets/space_img_3.png'
 import img13 from './assets/space_img_4.png'
 
+import img14 from './assets/razorpay_img.png'
+
 export const Images = () => {
   return (
     <div className='flex flex-wrap gap-3'>
@@ -37,6 +39,8 @@ export const Images = () => {
         <img src={img11} alt="" className='w-[180px] h-[180px]' />
         <img src={img12} alt="" className='w-[180px] h-[180px]' />
         <img src={img13} alt="" className='w-[180px] h-[180px]' />
+
+        <img src={img14} alt="" className='w-[180px] h-[180px]' />
 
     </div>
   )
